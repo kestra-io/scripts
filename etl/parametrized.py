63eb4957e0c164c476a4e382a7023a1af4afa493
Anna Geller <anna.m.geller@gmail.com>
2023-08-27 16:50:04 +0200

    add spark

--- etl/parametrized.py
+++ etl/parametrized.py
@@ -2,10 +2,8 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("num", type=int, help="Enter an integer")
+parser.add_argument("--num", type=int, default=42, help="Enter an integer")
 
-# Read arguments from command line
 args = parser.parse_args()
-
 result = args.num * 2
 print(result)

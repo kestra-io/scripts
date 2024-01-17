abdd856cb2c9ce4f073a980777b5e90f31bc366b
Anna Geller <anna.m.geller@gmail.com>
2024-01-17 17:12:36 +0100

    add flows

--- README.md
+++
@@ -1,3 +0,0 @@
-# Demo Scripts
-
-This repository includes various scripts for Blueprints and demos (Python, Node, R, Julia, Shell, Powershell, etc)
--- etl/parametrized.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument("--num", type=int, default=42, help="Enter an integer")
-
-args = parser.parse_args()
-result = args.num * 2
-print(result)
